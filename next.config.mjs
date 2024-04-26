f62783c07c4e2eee13ccffb5ce50657cdfe98e20
possicat <1yousefakrem@gmail.com>
2024-04-26 22:58:00 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  serverRuntimeConfig: {
+    timeout: 7200000,
+    serverTimeout: 7200000,
+  },
 };
 
 export default nextConfig;
